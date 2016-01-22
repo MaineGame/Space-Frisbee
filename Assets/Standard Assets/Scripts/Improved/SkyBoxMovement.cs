5d290932db6ad4b7ab291d7d74a5ad8389ff50a6
Quinten Campbell <qcampbell@utc4me.org>
2016-01-22 09:01:11 -0500

    a
    a

--- Assets/Standard Assets/Scripts/Improved/SkyBoxMovement.cs
+++ Assets/Standard Assets/Scripts/Improved/SkyBoxMovement.cs
@@ -5,8 +5,12 @@ public class SkyBoxMovement : MonoBehaviour {
 
     [SerializeField] float rotationSpeed;
 	
-	void LateUpdate () {
+    void Update()
+    {
         rotationSpeed += 0.25f * Time.deltaTime;
+    }
+	void LateUpdate () {
+        
         GetComponent<Skybox>().material.SetFloat("_Rotation", rotationSpeed);
 	}
 }
